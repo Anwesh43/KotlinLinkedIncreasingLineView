3d16c1bdfcee28a9f132b5c9f65494ffe6688481
Anwesh43 <anweshthecool0@gmail.com>
2018-06-26 03:09:46 +0530

    adding ILAnimator class

--- linkedincreasinglineview/src/main/java/com/anwesh/uiprojects/linkedincreasinglineview/LinkedIncreasingLineView.kt
+++ linkedincreasinglineview/src/main/java/com/anwesh/uiprojects/linkedincreasinglineview/LinkedIncreasingLineView.kt
@@ -45,4 +45,32 @@ class LinkedIncreasingLineView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class ILAnimator(var view : View, var animated : Boolean = false) {
+
+        fun start(cb : () -> Unit) {
+            if (animated) {
+                cb()
+                try {
+                    Thread.sleep(50)
+                    view.invalidate()
+                } catch(ex : Exception) {
+
+                }
+            }
+        }
+
+        fun start() {
+            if (!animated) {
+                animated = true
+                view.postInvalidate()
+            }
+        }
+
+        fun stop() {
+            if (animated) {
+                animated = false
+            }
+        }
+    }
 }
