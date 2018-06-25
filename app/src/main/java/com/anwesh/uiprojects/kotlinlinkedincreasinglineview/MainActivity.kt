4c68188ccb9718780ffe3f64bb32a29d67c72876
Anwesh43 <anweshthecool0@gmail.com>
2018-06-26 03:57:45 +0530

    adding OnCompletionListener in Activity

--- app/src/main/java/com/anwesh/uiprojects/kotlinlinkedincreasinglineview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/kotlinlinkedincreasinglineview/MainActivity.kt
@@ -3,13 +3,17 @@ package com.anwesh.uiprojects.kotlinlinkedincreasinglineview
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
+import android.widget.Toast
 import com.anwesh.uiprojects.linkedincreasinglineview.LinkedIncreasingLineView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        LinkedIncreasingLineView.create(this)
+        var view : LinkedIncreasingLineView = LinkedIncreasingLineView.create(this)
+        view.addCompletionListener {
+            Toast.makeText(this, "advanced to stage ${it + 1}", Toast.LENGTH_SHORT).show()
+        }
         fullScreen()
     }
 }
