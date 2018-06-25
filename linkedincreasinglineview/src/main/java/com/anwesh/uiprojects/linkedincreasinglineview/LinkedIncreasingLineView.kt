17499404383ebb357ecb85d690a21305e59af45d
Anwesh43 <anweshthecool0@gmail.com>
2018-06-26 03:26:43 +0530

    adding LinkedIncreasingLine class

--- linkedincreasinglineview/src/main/java/com/anwesh/uiprojects/linkedincreasinglineview/LinkedIncreasingLineView.kt
+++ linkedincreasinglineview/src/main/java/com/anwesh/uiprojects/linkedincreasinglineview/LinkedIncreasingLineView.kt
@@ -130,4 +130,28 @@ class LinkedIncreasingLineView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class LinkedIncreasingLine(var i : Int) {
+
+        private var curr : ILNode = ILNode(0)
+
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(stopcb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                stopcb(it)
+            }
+        }
+
+        fun startUpdating(startcb : () -> Unit) {
+            curr.startUpdating(startcb)
+        }
+    }
 }
