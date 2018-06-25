12d6dae283912d14cb72b9e69027c6114d9ab005
Anwesh43 <anweshthecool0@gmail.com>
2018-06-26 02:58:31 +0530

    adding LinkedIncreasingLineView class

--- linkedincreasinglineview/src/main/java/com/anwesh/uiprojects/linkedincreasinglineview/LinkedIncreasingLineView.kt
+++ linkedincreasinglineview/src/main/java/com/anwesh/uiprojects/linkedincreasinglineview/LinkedIncreasingLineView.kt
@@ -3,3 +3,26 @@ package com.anwesh.uiprojects.linkedincreasinglineview
 /**
  * Created by anweshmishra on 26/06/18.
  */
+import android.view.MotionEvent
+import android.content.Context
+import android.view.View
+import android.graphics.Paint
+import android.graphics.Canvas
+
+class LinkedIncreasingLineView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
